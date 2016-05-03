fd01b53a9bebc51b5687e7f1d4f01e27752d9d43
alanrhannah <alan@queryclick.com>
2016-05-03 11:40:25 +0100

    Updated tests to cover duration, check capturing outliers.

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,7 @@ import requests
 import requests_mock
 import unittest
 
+from decimal import Decimal, getcontext
 from overwatch import settings
 from types import DictType
 
@@ -91,6 +92,55 @@ class TestParseResponse(unittest.TestCase):
         for row in self.response.json()['finished']:
             self.assertEqual(row.keys(), keys)
 
+class TestCalculateTimeDifferences(unittest.TestCase):
+
+    def create_file_path(self, file_name):
+        path = os.path.join(self.data_file_path, file_name)
+        return path 
+
+    def setUp(self):
+        self.data_file_path = os.path.join(os.getcwd(), 'test_data')
+        self.json_file_name = 'scrapyd_list_jobs_response_json.json'
+        self.json_outliers_file_name = 'scrapyd_list_jobs_outliers_json.json'
+
+        self.scrapyd_list_jobs_response_json_path = self.create_file_path(
+            self.json_file_name)
+
+        self.scrapyd_outliers_json_path = self.create_file_path(
+            self.json_outliers_file_name)
+
+        self.scrapyd_list_jobs_response_json = open(
+            self.scrapyd_list_jobs_response_json_path, 'rb').read()
+
+        self.scrapyd_outliers_json = open(
+            self.scrapyd_outliers_json_path, 'rb').read()
+
+        self.response_json_dict = json.loads(
+            self.scrapyd_list_jobs_response_json)
+
+        self.outliers_json_dict = json.loads(
+            self.scrapyd_outliers_json)
+
+        self.calculated_delta = (self.parse_to_datetime(
+                self.response_json_dict['finished'][0]['end_time']
+                ) - self.parse_to_datetime(
+                self.response_json_dict['finished'][0]['start_time']
+                ))
+
+        self.earliest_datetime = None
+        self.latest_datetime = None
+
+        for item in self.outliers_json_dict['finished']:
+            if self.earliest_datetime is None:
+                self.earliest_datetime = self.parse_to_datetime(item['start_time'])
+            elif self.parse_to_datetime(item['start_time']) < self.earliest_datetime:
+                self.earliest_datetime = self.parse_to_datetime(item['start_time'])
+
+            if self.latest_datetime is None:
+                self.latest_datetime = self.parse_to_datetime(item['end_time'])
+            elif self.parse_to_datetime(item['end_time']) > self.latest_datetime:
+                self.latest_datetime = self.parse_to_datetime(item['end_time'])
+
     def parse_to_datetime(self, date_string):
         dt = datetime.datetime.strptime(
                 date_string,
@@ -102,17 +152,33 @@ class TestParseResponse(unittest.TestCase):
         expected = datetime.datetime(2016, 04, 29, 10, 28, 8, 4732)
         self.assertEqual(
             self.parse_to_datetime(
-                self.response.json()['finished'][0]['start_time']), expected)
+                self.response_json_dict['finished'][0]['start_time']),
+            expected)
 
     def test_subtract_start_end_datetime(self):
         expected = datetime.timedelta(0, 241, 547793)
-        self.assertEqual(
-            (self.parse_to_datetime(
-                self.response.json()['finished'][0]['end_time']
-                ) -
-            self.parse_to_datetime(
-                self.response.json()['finished'][0]['start_time']
-                ), expected))
+        self.assertEqual(self.calculated_delta, expected)
+
+    def test_convert_subtraction_to_minutes(self):
+        expected = 4.026
+        getcontext.prec = 4
+        self.calculated_mins = round(
+            Decimal(
+                self.calculated_delta.total_seconds()) / Decimal(60),
+            3)
+        self.assertEqual(self.calculated_mins, expected)
+
+    def test_collect_outliers(self):
+        self.assertEqual(self.earliest_datetime, 
+                         datetime.datetime(2016, 4, 29, 10, 28, 8, 4732))
+
+        self.assertEqual(self.latest_datetime,
+                         datetime.datetime(2016, 4, 29, 10, 33, 51, 420786))
+
+    def test_calculate_total_crawl_duration(self):
+        duration = self.latest_datetime - self.earliest_datetime
+        self.assertEqual(duration.total_seconds(), 343.416054)
+        pass
 
 if __name__ == '__main__':
     unittest.main()
